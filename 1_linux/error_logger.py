0ba50eaac7ee5140a98825e777f0e11b8bb922c8
Luciano Strika <lucianostrika44@gmail.com>
2018-07-25 18:17:18 -0300

    agregar error logger-

--- 1_linux/error_logger.py
+++ 1_linux/error_logger.py
@@ -4,9 +4,13 @@ e = 0
 while True:
     e+=1
     r = random.random()
-    if r > 0.2:
-        with open('errors_log', 'a') as log:
-            log.write("\n ERROR: {}".format(e))
+
+    with open('errors_log', 'a') as log:
+        if r > 0.4:
+                log.write("\n ERROR: {}".format(e))
+        else:
+                log.write("\n OK: {}".format(e))
+
     time.sleep(5)
 
 
