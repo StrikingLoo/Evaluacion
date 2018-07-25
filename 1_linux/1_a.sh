ad309ed77b2302607f0e77e9264053a353338e28
Luciano Strika <lucianostrika44@gmail.com>
2018-07-24 21:37:48 -0300

    agregar comentario

--- 1_linux/1_a.sh
+++ 1_linux/1_a.sh
@@ -1,2 +1,4 @@
 #El primer argumento es el filename para buscar la palabra Unix
+#Si sacaramos el -o, veriamos cada linea en la que aparece la palabra, 
+#Sin contar cada aparicion individual.
 cat $1|grep -o 'Unix'|wc -l
